9f1182c96a91034bc056ce286773f8e1257a5aaf
yiceth29 <yicethcarriazonarvaez@gmail.com>
2021-07-23 20:38:44 -0500

    solucion quiz bootstrap

---
+++ ClaseJavascript/Script.js
@@ -0,0 +1,53 @@
+/*Variables
+let condition = true
+const condition = true
+
+Tipos de datos
+number = 13
+string = `` siempre va entre comillas dobles sencillas o tildes al reves "puede ir cualquier texto y adicional podemos ingresar alguna clase ej: ${number]"
+boolean = true o false solo son esas dos opciones
+array = ["", 45 {}, []]
+JSON = {
+    key: []
+}*/
+
+//Condicionales
+
+// =declaras un valor //
+/*
+function suma(num1, num2) {
+    console.log(num1 + num2)
+}
+
+suma(5, 2)*/
+
+
+/*function unirNombres(nombres, apellidos){
+    let nombreCompleto = `${nombres} ${apellidos}`
+    //let nombreCompleto = nombres + '' + apellidos//
+    console.log(nombreCompleto)
+}
+
+unirNombres("Yiceth zamira", "Carriazo Narvaez")*/
+
+/*let descripcion = {nombre: "yiceth" , apellido: "carriazo" , edad: "27" , telefono:"3143033397"}
+console.log(descripcion)*/
+
+function positiveNegative (number){
+
+    if (number > 1){
+        console.log("El numero es positivo")
+    }
+    if (number < 1) {
+        console.log("El numero es negativo")
+    }
+    if (number === 0) {
+        console.log("El numero es Cero")
+    }
+}
+
+    positiveNegative(4)
+
+
+    
+    
